a32721df87be86ad48d27e7aa5fb15758170cde3
cyoon1729 <chrisyoon1729@gmail.com>
2022-11-21 22:14:46 -0500

    Orlang

---
+++ Orlang/ast.mli
@@ -0,0 +1,49 @@
+type operator = Add | Sub | Mlt | Div | Mod
+
+type lvalue =
+  | LVar of string
+
+type typ = 
+    Concrete of string
+  | TypVar of string
+  | ArrowTyp of typ * typ
+
+type expr =
+    IntLit of int
+  | BoolLit of int
+  | Var of string
+  | Call of hExpr * hExpr
+  | Lambda of lvalue * hExpr
+  | If of hExpr * hExpr * hExpr
+  | Let of binding * hExpr
+  | PatternMatch of hExpr * (patternRow list) 
+and hExpr = 
+    NoHint of expr
+  | Hint of expr * typ
+and binding = 
+    Binding of lvalue * hExpr * bool
+and patternRow = 
+  | PatternRow of pattern * hExpr
+and pattern = 
+    PatWildCard
+  | PatId of string
+  | PatLit of int
+  | PatCon of string * (hExpr list)
+
+
+type scheme =
+  | Scheme of string list * typ
+
+(* semantically checked expression; everything in orlang is an exprssion *)
+type sExpr = typ * sx 
+and sx =
+    SIntLit of int
+  | SBoolLit of int
+  | SVar of string
+  | SCall of sExpr * sExpr
+  | SLambda of lvalue * sExpr
+  | SIf of sExpr * sExpr * sExpr
+  | SLet of sbinding * sExpr
+and sbinding = 
+    SBinding of lvalue * sExpr * bool
+
---
+++ Orlang/unification.mli
@@ -0,0 +1,28 @@
+open Ast
+
+type substitution = typ Map.Make(String).t
+type typeEnvironm = scheme Map.Make(String).t
+type evalResult = 
+  { code: string;
+    tp  : typ;
+    sexpr: sExpr;
+    sub : substitution;
+  }
+
+(* apply a substitution to a type *)
+val apply : substitution -> typ -> typ
+
+(* apply a substitution to a type environment *)
+val applyte : substitution -> typeEnvironm -> typeEnvironm 
+
+(* compose two substitutions; sub1 gets applied before sub2 *)
+val compose : substitution -> substitution -> substitution
+
+(* checks if a type variable occurs in a type *)
+val occurs : string -> typ -> bool
+
+(* converts a type to a string for pretty print *)
+val toString : typ -> string
+
+(* unify two types; return the unified type and the substitution *)
+val unification : typ -> typ -> (typ * substitution)
